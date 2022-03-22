1bb65a0e326c71913e2dc3c5a68123e33ff5cd52
Laraa31 <88662021+Laraa31@users.noreply.github.com>
2022-03-22 13:16:18 +0100

    Add files via upload

---
+++ nasljedivanjeGeometrijskoTijelo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nasljedivanjeGeometrijskoTijelo
+{
+    public class GeometrijskoTijelo
+    {
+        double volumen, oplosje;
+        public static int brojacTijela = 0;
+
+        public GeometrijskoTijelo()
+        {
+            brojacTijela++;
+        }
+
+    }
+
+    public class Kugla : GeometrijskoTijelo
+    {
+        
+    }
+
+    public class Kvadar : GeometrijskoTijelo
+    {
+
+    }
+
+    public class Kocka : Kvadar
+    {
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kugla x = new Kugla();
+            Console.WriteLine(GeometrijskoTijelo.brojacTijela);
+            Kvadar y = new Kvadar();
+            Console.WriteLine(GeometrijskoTijelo.brojacTijela);
+            Kocka z = new Kocka();
+            Console.WriteLine(GeometrijskoTijelo.brojacTijela);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
